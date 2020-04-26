dc9bc58b38af0c593170b0e90fd191d01424a7c9
dkorosi <d.korosi96@gmail.com>
2020-04-26 14:46:23 +0200

    Multiplayer oldal is megcsinálva, csúszka ugyan az mint előtte(u.a a változó) Név, IP beállítás működik.

--- src/gui/GuiController.java
+++ src/gui/GuiController.java
@@ -4,16 +4,24 @@ import gamelogic.GameLoop;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 
 public class GuiController {
-
+    @FXML
+    private Slider timerSlider;
     @FXML
     private ToggleGroup startingColorTG;
-
     @FXML
     private ToggleGroup difficultyTG;
+    @FXML
+    private TextField multiNameText;
 
+    @FXML
+    private TextField multiIPAddrText;
     Canvas canvas;
 
     public ToggleGroup getStartingColorTG() {
@@ -24,9 +32,39 @@ public class GuiController {
     public ToggleGroup getDifficultyTG() {
         return difficultyTG;
     }
+
     @FXML
-    void startGame(ActionEvent event) {
+    void startSingleGame(ActionEvent event) {
         GameLoop game = new GameLoop(canvas);
+        System.out.println("Start Single GAME");
+    }
+    @FXML
+    void startLocalGame(ActionEvent event) {
+        String Name =   getMultiNameText();
+        GameLoop game = new GameLoop(canvas);
+        System.out.println("Start Local Game");
+        System.out.println("The name is:"+ Name);
+    }
+    @FXML
+    void startOnlineGame(ActionEvent event) {
+        String IPAddr = getMultiIPAddrText();
+        String Name =   getMultiNameText();
+        GameLoop game = new GameLoop(canvas);
+        System.out.println("Start Multi Game");
+        System.out.println("The name is: "+ Name + "\n" + "The IP Address is: " + IPAddr);
+    }
+    @FXML
+    double getTimerSliderVal(MouseEvent event) {
+        System.out.println("Slide");
+        return timerSlider.getValue();
     }
 
+    @FXML
+    String getMultiNameText() {
+        return multiNameText.getText();
+    }
+    @FXML
+    String getMultiIPAddrText() {
+        return multiIPAddrText.getText();
+    }
 }
