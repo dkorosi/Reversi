6bfcc35b0a70ebb4daaca263b607ac42f2fab0ed
dkorosi <d.korosi96@gmail.com>
2020-04-27 20:29:13 +0200

    kis icon hozáadása bal felülre

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,6 +18,7 @@ public class Main extends Application {
         window.setResizable(false);
         window.setScene(new Scene(root, 654, 460));
         window.show();
+        window.getIcons().add(new Image("gui/reversi_img.png"));
 
     }
 
